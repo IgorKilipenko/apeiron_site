317a08e2caecc3eb543d578236f2430df265cdb9
IgorKilipenko <igorkili@mail.ru>
2018-03-29 02:12:09 +0700

    api-config

--- api-config.js
+++ api-config.js
@@ -6,7 +6,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname.match(/localhost(:[0-9])*/ig)) {
   backendHost = 'http://localhost:3300';
 } else {
-  backendHost = hostname;
+  backendHost = 'http://' + hostname;
 }
 
 export const API_ROOT = `${backendHost}/graphql`;
