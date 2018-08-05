84513c81a3d1b28420b68e36fe4504e4ebbec931
IgorKilipenko <igorkili@mail.ru>
2018-08-05 14:10:07 +0700

    dev hot reload

--- server.js
+++ server.js
@@ -31,6 +31,17 @@ if (isDevelopment) {
             publicPath: webpackConfig.output.publicPath
         })
     );
+    app.use('*', (req, res, next) => {
+        const filename = path.resolve(compiler.outputPath, 'index.html');
+        compiler.outputFileSystem.readFile(filename, (err, result) => {
+            if (err) {
+                return next(err);
+            }
+            res.set('content-type', 'text/html');
+            res.send(result);
+            res.end();
+        });
+    });
 } else {
     app.use(express.static(PUBLIC_PATH));
 }
